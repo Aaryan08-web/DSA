48c31e5ea5c1183a5c4d9a60bbc265b81ef67ad0
Aaryan <chippaaaryan@gmail.com>
2026-08-19 20:17:12 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.63%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+// Aaryan08
+    void moveZeroes(vector<int>& nums) {
+        int temp = 0;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(nums[i] != 0 ){
+                swap(nums[i] , nums[temp]);
+                temp++;
+            }
+        }
+    }
+};
